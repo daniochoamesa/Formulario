1c52668ef64c7cf14d6712b75cf1b2fc626b7cff
daniochoamesa <32293791+daniochoamesa@users.noreply.github.com>
2018-02-12 17:04:38 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -21,4 +21,28 @@ function w3_close() {
     mySidebar.style.display = "none";
     overlayBg.style.display = "none";
 }
+
+// fichoro xml que está en el servidor rawgit
+var url="https://rawgit.com/urbinapro/jsxml/master/xml/preguntas.xml";
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+ if (this.readyState == 4 && this.status == 200) {
+  // función personalizada que gestiona la respuesta a la petición de fichero
+  gestionarXml(this); 
+ }
+};
+xhttp.open("GET", url, true); //url del fichero
+xhttp.send();
+
+// función personalizada que gestiona la respuesta a la petición de fichero
+function gestionarXml(dadesXml){
+  var xmlDoc = dadesXml.responseXML;
+  document.getElementById("p1").innerHTML = xmlDoc.getElementsByTagName("title")[2].childNodes[0].nodeValue;
+  document.getElementById("t1").innerHTML = xmlDoc.getElementsByTagName("type")[2].childNodes[0].nodeValue;
+  document.getElementById("r1").innerHTML = xmlDoc.getElementsByTagName("answer")[2].childNodes[0].nodeValue;
+
+
+}
+
 </script>
